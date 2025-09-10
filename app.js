d422c8858f69ca6b4a958a10b74f3a056bb15050
apni-coding <apnicoding72@gmail.com>
2025-09-10 14:58:52 +0530

    cors allow

--- app.js
+++ app.js
@@ -15,16 +15,7 @@ const allowedOrigins = [
   "https://jobportalv2.onrender.com"
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
+app.use(cors());
 
 app.use("/api", routes);
 
