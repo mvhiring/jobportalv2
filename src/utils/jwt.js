2421f07b470960c1e3aec3dce3c5b17114188af0
apni-coding <apnicoding72@gmail.com>
2025-09-19 12:07:21 +0530

    token time increase

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
-const JWT_EXPIRES_IN = "1m"; // login token expiry
+const JWT_EXPIRES_IN = "5h"; // login token expiry
 
 exports.generateToken = (payload) => {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
